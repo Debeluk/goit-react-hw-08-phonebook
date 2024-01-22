5d03a3dfe4be88cd4d9a4406d15dcbe74f6c0f26
Debeluk <popcorn.starinets@gmail.com>
2024-01-22 19:18:32 +0200

    test7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
 import LoginPage from './Navigation/Login';
 import RegisterPage from './Navigation/Registration';
 import PrivatePhonebook from './Navigation/PrivatePhonebook';
@@ -11,14 +11,12 @@ import HomePage from './Navigation/Homepage';
 const App = () => {
   return (
     <Provider store={store}>
-      <Router>
         <Routes>
           <Route path='/' component={HomePage} />
           <Route path="/login" component={LoginPage} />
           <Route path="/registration" component={RegisterPage} />
           <Route path="/contacts" component={PrivatePhonebook} />
         </Routes>
-      </Router>
     </Provider>
   );
 };
