e2fe6ea957e0cba6196b6efa2e69010a6df9bec1
Debeluk <popcorn.starinets@gmail.com>
2024-01-22 17:34:21 +0200

    1.5

--- src/components/Navigation/Login.js
+++ src/components/Navigation/Login.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
 import axios from 'axios';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const dispatch = useDispatch();
 
   const handleChange = (event) => {
     const { name, value } = event.target;
